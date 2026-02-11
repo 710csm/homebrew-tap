091616e5faf687d18398f0dea9f40fd8fee473a5
ChoiSeungMyeong <710csm@naver.com>
2026-02-11 14:54:21 +0900

    chore: update url for v0.1.1

--- Formula/reli.rb
+++ Formula/reli.rb
@@ -1,7 +1,7 @@
 class Reli < Formula
   desc "AI-powered Swift refactoring linter for iOS projects"
   homepage "https://github.com/710csm/Reli"
-  url "https://github.com/710csm/Reli/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/710csm/Reli/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "e7027e73c82d991c651e3045b36a7865c38f26645c85b49aaa00f2c2fe5fb000"
   license "MIT"
 
