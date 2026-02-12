9d5b4ba7c094e73e19408953d5f90432092ae1dc
ChoiSeungMyeong <710csm@naver.com>
2026-02-12 11:30:33 +0900

    chore: update url and sha256 for v0.3.0

--- Formula/reli.rb
+++ Formula/reli.rb
@@ -1,8 +1,8 @@
 class Reli < Formula
   desc "AI-powered Swift refactoring linter for iOS projects"
   homepage "https://github.com/710csm/Reli"
-  url "https://github.com/710csm/Reli/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "cac6c80874441fd088946af9b58686f6bdd3c1b689dabf60d062f4473519e5ea"
+  url "https://github.com/710csm/Reli/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "6b90c3726d60df35374faa501cefa7230ade59d7a5d76a5ed4a7780476e6bc07"
   license "MIT"
 
   depends_on :macos
